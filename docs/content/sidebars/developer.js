101b16e2c1e64b3b28ca303be63b84d59ab4ef68
Gino <ginowine1@gmail.com>
2025-06-24 10:50:03 +0100

    feat(docs): add validating a university degree tutorial to docs

--- docs/content/sidebars/developer.js
+++ docs/content/sidebars/developer.js
@@ -331,6 +331,7 @@ const developer = [
             },
             'developer/tutorials/live-concert',
             'developer/tutorials/retail-store',
+            'developer/tutorials/validate-university-degree',
         ],
     },
     {
