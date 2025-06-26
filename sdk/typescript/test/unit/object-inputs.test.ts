85558752de902bf51cefed7a4779a524dfbfb047
Panteleymonchuk <panteleymonchuk@gmail.com>
2025-06-26 16:13:52 +0300

    upstream(ts-sdk): [ts-sdk] Add tx.object.option for creatnig object options in transact…
    # Description of change
    
    Upstream change of
    https://github.com/MystenLabs/sui/commit/d5a23d78a9b30ec1c5ae5ac3cafdbe2817d68df6
    
    Upstream PR: https://github.com/MystenLabs/sui/pull/20194

--- sdk/typescript/test/unit/object-inputs.test.ts
+++ sdk/typescript/test/unit/object-inputs.test.ts
@@ -35,150 +35,228 @@ describe('Transaction inputs', () => {
                 tx.object.clock(),
                 tx.object.random(),
                 tx.object.denyList(),
+                tx.object.option({
+                    type: '0x123::example::Thing',
+                    value: '0x456',
+                }),
+                tx.object.option({
+                    type: '0x123::example::Thing',
+                    value: tx.object('0x456'),
+                }),
+                tx.object.option({
+                    type: '0x123::example::Thing',
+                    value: null,
+                }),
             ],
         });
 
         expect(tx.getData()).toMatchInlineSnapshot(`
-			{
-			  "commands": [
-			    {
-			      "$kind": "MoveCall",
-			      "MoveCall": {
-			        "arguments": [
-			          {
-			            "$kind": "Input",
-			            "Input": 0,
-			            "type": "object",
-			          },
-			          {
-			            "$kind": "Input",
-			            "Input": 1,
-			            "type": "object",
-			          },
-			          {
-			            "$kind": "Input",
-			            "Input": 2,
-			            "type": "object",
-			          },
-			          {
-			            "$kind": "Input",
-			            "Input": 3,
-			            "type": "object",
-			          },
-			          {
-			            "$kind": "Input",
-			            "Input": 4,
-			            "type": "pure",
-			          },
-			          {
-			            "$kind": "Input",
-			            "Input": 5,
-			            "type": "object",
-			          },
-			          {
-			            "$kind": "Input",
-			            "Input": 6,
-			            "type": "object",
-			          },
-			          {
-			            "$kind": "Input",
-			            "Input": 7,
-			            "type": "object",
-			          },
-			          {
-			            "$kind": "Input",
-			            "Input": 8,
-			            "type": "object",
-			          },
-			        ],
-			        "function": "bar",
-			        "module": "foo",
-			        "package": "0x0000000000000000000000000000000000000000000000000000000000000002",
-			        "typeArguments": [],
-			      },
-			    },
-			  ],
-			  "expiration": null,
-			  "gasData": {
-			    "budget": null,
-			    "owner": null,
-			    "payment": null,
-			    "price": null,
-			  },
-			  "inputs": [
-			    {
-			      "$kind": "UnresolvedObject",
-			      "UnresolvedObject": {
-			        "objectId": "0x0000000000000000000000000000000000000000000000000000000000000123",
-			      },
-			    },
-			    {
-			      "$kind": "Object",
-			      "Object": {
-			        "$kind": "Receiving",
-			        "Receiving": {
-			          "digest": "4vJ9JU1bJJE96FWSJKvHsmmFADCg4gpZQff4P3bkLKi",
-			          "objectId": "0x0000000000000000000000000000000000000000000000000000000000000001",
-			          "version": "123",
-			        },
-			      },
-			    },
-			    {
-			      "$kind": "Object",
-			      "Object": {
-			        "$kind": "SharedObject",
-			        "SharedObject": {
-			          "initialSharedVersion": "123",
-			          "mutable": true,
-			          "objectId": "0x0000000000000000000000000000000000000000000000000000000000000002",
-			        },
-			      },
-			    },
-			    {
-			      "$kind": "Object",
-			      "Object": {
-			        "$kind": "ImmOrOwnedObject",
-			        "ImmOrOwnedObject": {
-			          "digest": "4vJ9JU1bJJE96FWSJKvHsmmFADCg4gpZQff4P3bkLKi",
-			          "objectId": "0x0000000000000000000000000000000000000000000000000000000000000003",
-			          "version": "123",
-			        },
-			      },
-			    },
-			    {
-			      "$kind": "Pure",
-			      "Pure": {
-			        "bytes": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI=",
-			      },
-			    },
-			    {
-			      "$kind": "UnresolvedObject",
-			      "UnresolvedObject": {
-			        "objectId": "0x0000000000000000000000000000000000000000000000000000000000000005",
-			      },
-			    },
-			    {
-			      "$kind": "UnresolvedObject",
-			      "UnresolvedObject": {
-			        "objectId": "0x0000000000000000000000000000000000000000000000000000000000000006",
-			      },
-			    },
-			    {
-			      "$kind": "UnresolvedObject",
-			      "UnresolvedObject": {
-			        "objectId": "0x0000000000000000000000000000000000000000000000000000000000000008",
-			      },
-			    },
-			    {
-			      "$kind": "UnresolvedObject",
-			      "UnresolvedObject": {
-			        "objectId": "0x0000000000000000000000000000000000000000000000000000000000000403",
-			      },
-			    },
-			  ],
-			  "sender": null,
-			  "version": 2,
-			}
-		`);
+					{
+						"commands": [
+							{
+								"$kind": "MoveCall",
+								"MoveCall": {
+									"arguments": [
+										{
+											"$kind": "Input",
+											"Input": 9,
+											"type": "object",
+										},
+									],
+									"function": "some",
+									"module": "option",
+									"package": "0x0000000000000000000000000000000000000000000000000000000000000001",
+									"typeArguments": [
+										"0x123::example::Thing",
+									],
+								},
+							},
+							{
+								"$kind": "MoveCall",
+								"MoveCall": {
+									"arguments": [
+										{
+											"$kind": "Input",
+											"Input": 9,
+											"type": "object",
+										},
+									],
+									"function": "some",
+									"module": "option",
+									"package": "0x0000000000000000000000000000000000000000000000000000000000000001",
+									"typeArguments": [
+										"0x123::example::Thing",
+									],
+								},
+							},
+							{
+								"$kind": "MoveCall",
+								"MoveCall": {
+									"arguments": [],
+									"function": "none",
+									"module": "option",
+									"package": "0x0000000000000000000000000000000000000000000000000000000000000001",
+									"typeArguments": [
+										"0x123::example::Thing",
+									],
+								},
+							},
+							{
+								"$kind": "MoveCall",
+								"MoveCall": {
+									"arguments": [
+										{
+											"$kind": "Input",
+											"Input": 0,
+											"type": "object",
+										},
+										{
+											"$kind": "Input",
+											"Input": 1,
+											"type": "object",
+										},
+										{
+											"$kind": "Input",
+											"Input": 2,
+											"type": "object",
+										},
+										{
+											"$kind": "Input",
+											"Input": 3,
+											"type": "object",
+										},
+										{
+											"$kind": "Input",
+											"Input": 4,
+											"type": "pure",
+										},
+										{
+											"$kind": "Input",
+											"Input": 5,
+											"type": "object",
+										},
+										{
+											"$kind": "Input",
+											"Input": 6,
+											"type": "object",
+										},
+										{
+											"$kind": "Input",
+											"Input": 7,
+											"type": "object",
+										},
+										{
+											"$kind": "Input",
+											"Input": 8,
+											"type": "object",
+										},
+										{
+											"$kind": "Result",
+											"Result": 0,
+										},
+										{
+											"$kind": "Result",
+											"Result": 1,
+										},
+										{
+											"$kind": "Result",
+											"Result": 2,
+										},
+									],
+									"function": "bar",
+									"module": "foo",
+									"package": "0x0000000000000000000000000000000000000000000000000000000000000002",
+									"typeArguments": [],
+								},
+							},
+						],
+						"expiration": null,
+						"gasData": {
+							"budget": null,
+							"owner": null,
+							"payment": null,
+							"price": null,
+						},
+						"inputs": [
+							{
+								"$kind": "UnresolvedObject",
+								"UnresolvedObject": {
+									"objectId": "0x0000000000000000000000000000000000000000000000000000000000000123",
+								},
+							},
+							{
+								"$kind": "Object",
+								"Object": {
+									"$kind": "Receiving",
+									"Receiving": {
+										"digest": "4vJ9JU1bJJE96FWSJKvHsmmFADCg4gpZQff4P3bkLKi",
+										"objectId": "0x0000000000000000000000000000000000000000000000000000000000000001",
+										"version": "123",
+									},
+								},
+							},
+							{
+								"$kind": "Object",
+								"Object": {
+									"$kind": "SharedObject",
+									"SharedObject": {
+										"initialSharedVersion": "123",
+										"mutable": true,
+										"objectId": "0x0000000000000000000000000000000000000000000000000000000000000002",
+									},
+								},
+							},
+							{
+								"$kind": "Object",
+								"Object": {
+									"$kind": "ImmOrOwnedObject",
+									"ImmOrOwnedObject": {
+										"digest": "4vJ9JU1bJJE96FWSJKvHsmmFADCg4gpZQff4P3bkLKi",
+										"objectId": "0x0000000000000000000000000000000000000000000000000000000000000003",
+										"version": "123",
+									},
+								},
+							},
+							{
+								"$kind": "Pure",
+								"Pure": {
+									"bytes": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI=",
+								},
+							},
+							{
+								"$kind": "UnresolvedObject",
+								"UnresolvedObject": {
+									"objectId": "0x0000000000000000000000000000000000000000000000000000000000000005",
+								},
+							},
+							{
+								"$kind": "UnresolvedObject",
+								"UnresolvedObject": {
+									"objectId": "0x0000000000000000000000000000000000000000000000000000000000000006",
+								},
+							},
+							{
+								"$kind": "UnresolvedObject",
+								"UnresolvedObject": {
+									"objectId": "0x0000000000000000000000000000000000000000000000000000000000000008",
+								},
+							},
+							{
+								"$kind": "UnresolvedObject",
+								"UnresolvedObject": {
+									"objectId": "0x0000000000000000000000000000000000000000000000000000000000000403",
+								},
+							},
+							{
+								"$kind": "UnresolvedObject",
+								"UnresolvedObject": {
+									"objectId": "0x0000000000000000000000000000000000000000000000000000000000000456",
+								},
+							},
+						],
+						"sender": null,
+						"version": 2,
+					}
+			`);
     });
 });
